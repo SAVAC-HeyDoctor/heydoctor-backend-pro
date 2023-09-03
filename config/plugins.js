9bd41f8bbe4a61bb641691ffa4a9cf1b2f294991
Johan Sebastián Hernández <25942141+jseb2520@users.noreply.github.com>
2023-09-03 13:15:15 -0500

    update plugins file with graphql depth limit increase

--- config/plugins.js
+++ config/plugins.js
@@ -20,18 +20,15 @@ module.exports = ({ env }) => ({
       endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: true,
-      depthLimit: 7,
-      amountLimit: 100,
+      depthLimit: 10,
+      amountLimit: 200,
       apolloServer: {
-        tracing: false,
+        tracing: true,
       },
     },
   },
   "apollo-sandbox": {
-    // enables the plugin only in development mode
-    // if you also want to use it in production, set this to true
-    // keep in mind that graphql playground has to be enabled
     enabled: true,
-    // endpoint: "https://tunneled-strapi.com/graphql", // OPTIONAL - endpoint has to be accessible from the browser
+    endpoint: "https://heydoctor-api.onrender.com/graphql",
   },
 });
